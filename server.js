e1c811ab12a59f8beb4d23eeed0566f25b8a82e8
sergio cano rodriguez <sergio@MacBook-Pro-de-sergio.local>
2018-01-12 19:28:48 -0600

    Version inicial

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var app = express()
+
+app.use(cors())
+app.use(express())
+
+var posts = [
+    { message: 'Hola' },
+    { message: 'Mundo' }
+]
+
+app.get( '/posts', (req, res ) =>{
+    res.send(posts)
+})
+
+app.post('/register', (req, res) => {
+    var userData = req.body;
+    console.log(userData.email);
+    res.sendStatus(200);
+})
+
+app.listen(3000);
