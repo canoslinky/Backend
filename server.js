d4ec3be0de0c79f90797765b94a8fdf5583a2d5b
Sergio Cano Rodriguez <scano@gbm.com.mx>
2018-01-22 11:35:43 -0600

    Mongoose

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var cors = require('cors')
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+
 var app = express()
 
 app.use(cors())
 app.use(express())
+app.use(bodyParser.json())
 
 var posts = [
     { message: 'Hola' },
@@ -21,4 +24,8 @@ app.post('/register', (req, res) => {
     res.sendStatus(200);
 })
 
+mongoose.connect('mongodb://test:test@ds211558.mlab.com:11558/psaccount',  (err) => {
+    if(!err)
+        console.log('connected to mongo')
+})
 app.listen(3000);
